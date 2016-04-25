60900783d878eaa68a16fb09ebff9f49f11ffcfe
Levente Balogh <balogh.levente.hu@gmail.com>
2016-04-25 12:43:40 +0200

    feat(mount-encrypted): add scripts to mount encrypted containers
    EventScripts will trigger these scripts when particular events occur.
    
    Signed-off-by: Levente Balogh <balogh.levente.hu@gmail.com>

---
+++ mount-encrypted-usb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+VOLUME_PATH=$1
+ENCFS="/usr/local/bin/encfs"
+ENCDIR="$VOLUME_PATH/Private"
+DECDIR="$HOME/Private"
+
+security find-generic-password -ga encfs 2>&1 >/dev/null | cut -d'"' -f2 | "$ENCFS" -S "$ENCDIR" "$DECDIR"
