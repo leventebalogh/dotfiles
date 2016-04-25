2fb62d329fa5565b8f673953bcad12ad80f7e67a
Levente Balogh <balogh.levente.hu@gmail.com>
2016-04-25 10:45:10 +0200

    feat(brew.sh): clear and update
    Clear binaries that are not so important right now.
    Also add some important ones.

--- brew.sh
+++ brew.sh
@@ -21,13 +21,8 @@ sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
 # Install some other useful utilities like `sponge`.
 brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
 brew install bash
 brew tap homebrew/versions
 brew install bash-completion2
@@ -47,36 +42,17 @@ brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
 brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
 brew install nmap
 brew install pngcheck
-brew install socat
 brew install sqlmap
 brew install tcpflow
 brew install tcpreplay
 brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
 
 # Install other useful binaries.
 brew install ack
 brew install dark-mode
-#brew install exiv2
 brew install git
-brew install git-lfs
 brew install imagemagick --with-webp
 brew install lua
 brew install lynx
@@ -84,13 +60,28 @@ brew install p7zip
 brew install pigz
 brew install pv
 brew install rename
-brew install rhino
 brew install speedtest_cli
 brew install ssh-copy-id
 brew install testssl
 brew install tree
 brew install webkit2png
-brew install zopfli
+brew install wget
+brew install htop
+brew install tig
+brew install tmux
+brew install reattach-to-user-namespace
+brew install python3
+
+# Install useful Applications
+brew cask install google-chrome
+brew cask install vlc
+brew cask install cheatsheet
+brew cask install transmission
+brew cask install skype
+brew cask install steam
+brew cask install spotify
+brew cask install osxfuse
+brew cask install encfs
 
 # Remove outdated versions from the cellar.
 brew cleanup
