bc34831d51b3de09e086ac535c9cb029ddbbab1f
Levente Balogh <balogh.levente.hu@gmail.com>
2016-04-25 13:36:35 +0200

    feat(install.sh): install Gitx

--- install.sh
+++ install.sh
@@ -91,6 +91,7 @@ brew cask install steam
 brew cask install spotify
 brew cask install osxfuse
 brew cask install encfs
+brew cask install gitx
 
 # Install Node JS
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | bash```
