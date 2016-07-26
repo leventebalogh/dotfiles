c7f0a3b359789c1d42b14710d3ee9daa643b837d
Levente Balogh <balogh.levente.hu@gmail.com>
2016-07-26 17:16:17 +0200

    feat(install): install xcode command line tools on start

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@
 # Ask for the administrator password upfront.
 sudo -v
 
+# Install XCode Commandline Tools
+xcode-select --install
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
