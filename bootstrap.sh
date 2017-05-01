1d7c0dbacf09e4d22a1ae782033230a848d945a6
Levente Balogh <balogh.levente.hu@gmail.com>
2017-05-01 17:57:39 +0200

    bootstrap.sh: update rsync command

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,19 +4,30 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
-function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+function overwrite() {
+	rsync --exclude ".git/" \
+        --exclude "vscode/" \
+        --exclude "scripts/" \
+        --exclude "osx/" \
+        --exclude "bootstrap.sh" \
+        --exclude ".DS_Store" \
+        --exclude ".gitignore" \
+        --exclude "README.md" \
+        -avh \
+        --no-perms \
+        . \
+        ~;
+
 	source ~/.bash_profile;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+	overwrite;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
+		overwrite;
 	fi;
 fi;
-unset doIt;
+unset overwrite;
